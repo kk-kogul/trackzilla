effc61470abb4d3bc172977c3f9d5c7f3ba57cfd
KK - Kogul <kkrishnasamy@iodinesoftware.com>
2023-03-25 09:42:12 +0530

    Repo created from Spring Initializr

---
+++ src/main/java/com/demo/trackzilla/TrackZillaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.trackzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackZillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackZillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/trackzilla/TrackZillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.trackzilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackZillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
